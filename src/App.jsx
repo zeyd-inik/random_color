8827f6153786701ea139e65e5b4e3c53df11e44e
zeyd <1zeydinik@gmail/com>
2024-09-04 05:14:14 +0200

    Add generateRandomHexColor feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,26 @@ import { useState } from 'react';
 import './app.css';
 
 function App() {
-  const [color, setColor] = useState('#000000');
+  const [color, setColor] = useState('#70d179');
+  const [colorType, setColorType] = useState('HEX');
+
+  const randomChar = (length) => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const handleRandomHexColor = () => {
+    let hex = '#';
+    const hexChars = [0, 1, 2, 3, 4, 5, 6, 'A', 'B', 'C', 'D', 'E', 'F'];
+    for (let i = 0; i < 6; i++) {
+      hex += hexChars[randomChar(hexChars.length)];
+    }
+    setColor(hex);
+  };
+  const handleRandomRgbColor = () => {};
+
+  const handleRandomColor = () => {
+    colorType === 'HEX' ? handleRandomHexColor() : handleRandomRgbColor();
+  };
 
   return (
     <div
@@ -11,9 +30,26 @@ function App() {
       }}
       className="app"
     >
-      <button>Create Hex Color</button>
-      <button>Create RGB Color</button>
-      <button>Generate Random Color</button>
+      <div className="button-container">
+        <button
+          onClick={() => {
+            setColorType('HEX');
+          }}
+        >
+          Create Hex Color
+        </button>
+        <button
+          onClick={() => {
+            setColorType('RGB');
+          }}
+        >
+          Create RGB Color
+        </button>
+        <button onClick={handleRandomColor}>Generate Random Color</button>
+      </div>
+
+      <div className="type-text">{`${colorType} Color`}</div>
+      <div className="text">{color}</div>
     </div>
   );
 }
